f596a62959e722f0011564c9f57f590be71162de
lifan96 <lifan96@qq.com>
2019-07-06 15:46:30 +0800

    otp验证码获取

--- src/main/java/cn/lifan/controller/UserController.java
+++ src/main/java/cn/lifan/controller/UserController.java
@@ -11,12 +11,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 @Controller("user")
 @RequestMapping("/user")
 public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户获取otp短信接口
+    @RequestMapping("/getotp")
+    @ResponseBody
+    public  CommonReturnType getOtp(@RequestParam(name="telphone")String telphone){
+
+        //需要按照一定的规则生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //将otp验证码同对应的手机号关联，使用httpsession的方式绑定他的手机号与OTPCODE
+        httpServletRequest.getSession().setAttribute(telphone,otpCode);
+
+        //将otp验证码通过短信通道发给用户(暂未实现)
+        System.out.println("telphone=" + telphone+"& otpcode="+otpCode);
+        return CommonReturnType.create(null);
+    }
+
+
 
     @RequestMapping("/get")
     @ResponseBody
