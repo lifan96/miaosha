7cbdc23cff3bfebb8f7a884e5b96cfaa3387967e
lifan96 <lifan96@qq.com>
2019-07-10 11:03:10 +0800

    商品详情页接口和列表页接口实现完成

--- src/main/java/cn/lifan/controller/viewobject/ItemVO.java
+++ src/main/java/cn/lifan/controller/viewobject/ItemVO.java
@@ -1,35 +1,28 @@
-package cn.lifan.service.model;
+package cn.lifan.controller.viewobject;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public class ItemModel {
+public class ItemVO {
+
     private  Integer id;
 
     //商品名
-    @NotBlank(message = "商品名称不能为空")
     private String title;
 
     //商品价格
-    @NotNull(message = "商品价格不能为空")
-    @Min(value = 0,message = "商品价格必须大于0")
     private BigDecimal price;
 
     //商品库存
-    @NotNull(message = "库存必须填写")
     private Integer stock;
 
     //商品描述
-    @NotBlank(message = "商品描述信息不能为空")
     private String description;
 
     //商品销量
     private Integer sales;
 
     //商品描述图片的URL
-    @NotBlank(message = "图片信息不能为空")
     private String imgUrl;
 
     public Integer getId() {
